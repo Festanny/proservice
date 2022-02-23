c350b0389d87fd4fb48a6ceabd1ebc96c898a9a9
Festanny <junior-format30@mail.ru>
2022-02-23 22:10:49 +0400

    upd

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
-$("body").on('click', '[href*="#"]', function(e){
-	var fixed_offset = $('header nav').height()+30;
-	$('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+// href
+$("body").on('click', '[href*="#"]', function (e) {
+	var fixed_offset = $('header nav').height() + 30;
+	$('html,body').stop().animate({
+		scrollTop: $(this.hash).offset().top - fixed_offset
+	}, 1000);
 	e.preventDefault();
 });
 
-$(window).scroll(function(){
-    if ($(window).scrollTop() > $('header').height()) {
-        $('header nav').addClass('fixed');
-    }
-    else {
-        $('header nav').removeClass('fixed');
-    }
+// scroll fixed nav
+$(window).scroll(function () {
+	if ($(window).scrollTop() > $('header').height()) {
+		$('header nav').addClass('fixed');
+	} else {
+		$('header nav').removeClass('fixed');
+	}
 });
+
+// animation block
+function onEntry(entry) {
+	entry.forEach(change => {
+		if (change.isIntersecting) {
+			change.target.classList.add('el-show');
+		}
+	});
+}
+
+let options = {
+	threshold: [0.1]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.el');
+
+for (let elm of elements) {
+	observer.observe(elm);
+}
