2bcecbc863c6cc7ab38dc1c38775feb5701c3d5f
Festanny <junior-format30@mail.ru>
2022-02-23 22:58:39 +0400

    add animation for header and all

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ function onEntry(entry) {
 }
 
 let options = {
-	threshold: [0.1]
+	threshold: [0.3]
 };
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('.el');
