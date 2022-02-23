5d732bd6596e8fd9e829b8b2a6f0afccaee2d04f
Festanny <junior-format30@mail.ru>
2022-02-23 21:59:03 +0400

    add jq

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 $("body").on('click', '[href*="#"]', function(e){
-	var fixed_offset = 0;
+	var fixed_offset = $('header nav').height()+30;
 	$('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
 	e.preventDefault();
 });
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() > $('header').height()) {
+        $('header nav').addClass('fixed');
+    }
+    else {
+        $('header nav').removeClass('fixed');
+    }
+});
+
+// $(window).scroll(function(){
+//     if ($('header nav').hasClass('fixed')) {
+//         $('header .header').addClass('paddingTopHeader');
+//     }
+//     else {
+//         $('header .header').removeClass('paddingTopHeader');
+//     }
+// });
+
